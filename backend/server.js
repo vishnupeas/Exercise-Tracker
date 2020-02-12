35faba65717adcef5ad4e255306fa4b08129a06f
woomiz <vishnups1998@gmail.com>
2020-02-12 20:40:45 +0530

    getting started with all the serverside

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
-require("dovenv").config();
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 
+//connecting mongodb database
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+//using the express to listen to the port
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("server is running on port:${port}");
+  console.log(`server is running on port:${port}`);
 });
