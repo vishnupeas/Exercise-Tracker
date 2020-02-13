9100f2873787edad5df65f17bc3f887f546f80a7
woomiz <vishnups1998@gmail.com>
2020-02-13 18:19:22 +0530

    added in a collapsable bootstrap 4 navbar

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -4,24 +4,47 @@ import { Link } from "react-router-dom";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/">
           Exercise-Tracker
         </Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="navbar-item">
-              <Link to="/" className="nav-link">
-                Exercises
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li
+              className="nav-item active"
+              data-toggle="collapse"
+              data-target=".navbar-collapse.show"
+            >
+              <Link className="nav-link" to="/">
+                Exercises<span className="sr-only">(current)</span>
               </Link>
             </li>
-            <li className="navbar-item">
-              <Link to="/create" className="nav-link">
+            <li
+              className="nav-item"
+              data-toggle="collapse"
+              data-target=".navbar-collapse.show"
+            >
+              <Link className="nav-link" to="/create">
                 Create Exercise Log
               </Link>
             </li>
-            <li className="navbar-item">
-              <Link to="/user" className="nav-link">
+            <li
+              className="nav-item"
+              data-toggle="collapse"
+              data-target=".navbar-collapse.show"
+            >
+              <Link className="nav-link" to="/user">
                 Create User
               </Link>
             </li>
