f23fcc1443016c5ff34f168009ea0621fde44176
woomiz <vishnups1998@gmail.com>
2020-02-13 14:46:21 +0530

    creating form 4 creating both users and exercises

--- src/components/Create-exercise.component.js
+++ src/components/Create-exercise.component.js
@@ -1,10 +1,107 @@
 import React, { Component } from "react";
+import InfiniteCalendar from "react-infinite-calendar";
+import "react-infinite-calendar/styles.css";
 
 export default class CreateExercise extends Component {
+  state = { users: [], username: "", description: "", duration: "", date: "" };
+
+  componentDidMount() {
+    this.setState({ users: ["test_user"], username: "test_user" });
+  }
+
+  onChangeUserName = e => {
+    this.setState({ username: e.target.value });
+  };
+
+  onChangeDescription = e => {
+    this.setState({ description: e.target.value });
+  };
+
+  onChangeDuration = e => {
+    this.setState({ duration: e.target.value });
+  };
+
+  onChangeDate = date => {
+    this.setState({ date: date });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
+    };
+    console.log(exercise);
+
+    window.location = "/";
+  };
+
   render() {
     return (
       <div>
-        <p>You are on the CreateExercise component!</p>
+        <h3>Create New Exercise Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username:</label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function(user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Description:</label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Duration (in minutes): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <InfiniteCalendar
+              minDate={
+                new Date(2020, new Date().getMonth(), new Date().getDate())
+              }
+              onSelect={this.onChangeDate}
+              displayOptions={{
+                showMonthsForYears: false
+              }}
+            />
+          </div>
+          <br />
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Exercise Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
       </div>
     );
   }
--- src/components/Create-user.component.js
+++ src/components/Create-user.component.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
 
 export default class CreateUser extends Component {
+  state = { username: "" };
+
+  componentDidMount() {
+    this.setState({ username: "new_username" });
+  }
+
+  onChangeUserName = e => {
+    this.setState({ username: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username
+    };
+    console.log(user);
+
+    this.setState({
+      username: ""
+    });
+  };
+
   render() {
     return (
       <div>
-        <p>You are on the CreateUser component!</p>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Enter the Username: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUserName}
+            />
+          </div>
+
+          <div className="form-group">
+            <input type="submit" value="Create" className="btn btn-success" />
+          </div>
+        </form>
       </div>
     );
   }
