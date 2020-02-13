0b5cb883f5bd7a01e5ea1fa4370d06eb8d0f112f
woomiz <vishnups1998@gmail.com>
2020-02-13 10:52:34 +0530

    creating a sample mockup for the Routers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Navbar from "./components/Navbar.component";
+import ExercisesList from "./components/Exercises-list.component";
+import EditExercise from "./components/Edit-exercises.component";
+import CreateExercise from "./components/Create-exercise.component";
+import CreateUser from "./components/Create-user.component";
+
 function App() {
   return (
     <Router>
